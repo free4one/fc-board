de8565d44639a4a828618f67b1e8ecf66e414447
hskim <hskim@sptek.co.kr>
2024-03-29 16:42:03 +0900

    feat: Configure application.yml

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,17 +27,17 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.4.0")
-//    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("com.querydsl:querydsl-jpa:5.1.0:jakarta")
-//    kapt("com.querydsl:querydsl-apt:5.1.0:jakarta")
-    runtimeOnly("com.h2database:h2")
+    kapt("com.querydsl:querydsl-apt:5.1.0:jakarta")
     runtimeOnly("com.mysql:mysql-connector-j")
+    testImplementation("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.kotest:kotest-runner-junit5:5.8.1")
     testImplementation("io.kotest:kotest-assertions-core:5.8.1")
     testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
-//    testImplementation("io.kotest.extensions:kotest-extensions-testcontainers:2.0.2")
-//    testImplementation("org.apache.commons:commons-compress:1.26.1")
+    testImplementation("io.kotest.extensions:kotest-extensions-testcontainers:2.0.2")
+    testImplementation("org.apache.commons:commons-compress:1.26.1")
 }
 
 tasks.withType<KotlinCompile> {
