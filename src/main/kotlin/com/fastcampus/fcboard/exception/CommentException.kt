a28704cf3f898c3d89309077c01aee0053907129
hskim <hskim@sptek.co.kr>
2024-04-08 17:31:24 +0900

    feat: Add create update delete functions for comments

---
+++ src/main/kotlin/com/fastcampus/fcboard/exception/CommentException.kt
@@ -0,0 +1,17 @@
+package com.fastcampus.fcboard.exception
+
+/**
+ * <p></p>
+ *
+ * <ul>
+ *     <li>Updated on : 4/8/24</li>
+ *     <li>Updated by : spd2team</li>
+ * </ul>
+ */
+open class CommentException(message: String) : RuntimeException(message)
+
+class CommentNotUpdatableException : CommentException("댓글을 수정할 수 없습니다.")
+
+class CommentNotFoundException : CommentException("댓글을 찾을 수 없습니다.")
+
+class CommentNotDeletableException : CommentException("댓글을 삭제할 수 없습니다.")
